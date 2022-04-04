7856200676904ac3b20322c6daa25af7c2a9d1cd
me <bluejeansh@hanmail.net>
2022-04-04 11:32:11 +0900

    1 commit

---
+++ github_boot/src/main/java/edu/multi/boot1/GithubBootApplication.java
@@ -0,0 +1,30 @@
+package edu.multi.boot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubBootApplication.class, args);
+		System.out.println("깃헙fork용도");
+//		System.out.println("원격저장소 추가 라인");
+//		System.out.println("소스주인 추가 라인");
+//
+//		System.out.println("소스주인 또 추가 라인");	
+//		System.out.println("소스주인 또또 추가 라인");		
+//
+//		System.out.println("소스주인 또또또 추가 라인");
+//		System.out.println("팀원 추가 라인");
+//
+//	
+//		System.out.println("팀원 또 추가 라인");		
+//		System.out.println("소스주인 추가 라인4");
+//		System.out.println("팀원 추가 라인3");
+
+
+
+	}
+
+}
